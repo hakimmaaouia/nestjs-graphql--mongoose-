36ecbf25e247df53f828e3d185938737b9300da9
hakim maaouia <hakimmaaouia@gmail.com>
2021-04-25 16:27:15 +0100

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { LessenModule } from './lessen/lessen.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { StudentModule } from './student/student.module';
+import * as config from 'config'
 
-
+const dbconfig =config.get('db')
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://hakim:azertyuiop123@cluster0.ez7ju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+  imports: [MongooseModule.forRoot(`mongodb+srv://${dbconfig.user}:${dbconfig.password}@cluster0.ez7ju.mongodb.net/${dbconfig.databasename}?retryWrites=true&w=majority`),
   GraphQLModule.forRoot({
     autoSchemaFile:true,
   }),
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config'
 async function bootstrap() {
+  const serverConfig =config.get('server')
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(serverConfig.prot);
 }
 bootstrap();
